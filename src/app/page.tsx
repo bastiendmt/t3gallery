783ea36f873659d1c337991f35d7326f9c25dc1d
Bastien Dumont <bastienxs@gmail.com>
2024-04-16 18:20:52 +0200

    scaffold ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/faabc6d6-cf32-40ed-8994-8731dfb74f80-k68oyj.png",
+  "https://utfs.io/f/290fbe52-45c7-4a2f-8e57-4e62f1eb53d6-iwccy5.png",
+  "https://utfs.io/f/23380190-c17e-4fb8-95e9-0c7b10a0ab24-8pjb3t.png",
+  "https://utfs.io/f/aa1f229f-bf09-42bd-a4d7-23a283b1e041-9ee98j.48.49.png",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url: url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
