f5eccf0438df7245740c1700579a6069fca68b7a
Bastien Dumont <bastienxs@gmail.com>
2024-04-19 12:58:43 +0200

    move query into separate file

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,12 +1,5 @@
-import { drizzle } from "drizzle-orm/vercel-postgres";
 import { sql } from "@vercel/postgres";
-import {
-  pgTable,
-  serial,
-  text,
-  timestamp,
-  uniqueIndex,
-} from "drizzle-orm/pg-core";
+import { drizzle } from "drizzle-orm/vercel-postgres";
 
 import * as schema from "./schema";
 
