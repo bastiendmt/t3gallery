7d68a65aaf108b81bf3008c0195ddd9cfeb6dc8f
Bastien Dumont <bastienxs@gmail.com>
2024-04-23 19:35:16 +0200

    fix user image

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -5,7 +5,7 @@ import { auth } from "@clerk/nextjs/server";
 export async function getMyImages() {
   const user = auth();
 
-  if (!user) throw new Error("Unauthorized");
+  if (!user.userId) throw new Error("Unauthorized");
 
   const images = await db.query.images.findMany({
     where: (model, { eq }) => eq(model.userId, user.userId),
